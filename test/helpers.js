c29268722a52e6d080bf9dcc112d9990a05fbf45
Simon Boudrias <admin@simonboudrias.com>
2013-10-17 23:05:28 -0400

    Refactor and complement yeoman.test.createGenerator tests

--- test/helpers.js
+++ test/helpers.js
@@ -2,38 +2,41 @@
 
 var util = require('util');
 var assert = require('assert');
-var generators = require('..');
-var helpers = require('../').test;
+var yeoman = require('..');
+var helpers = yeoman.test;
 
-describe('yeoman.generators.test', function () {
+describe('yeoman.test', function () {
   'use strict';
-  var Unicorn;
 
   beforeEach(function () {
     var self = this;
-    Unicorn = function (args, options) {
+    this.StubGenerator = function (args, options) {
       self.args = args;
       self.options = options;
-      generators.Base.apply(this, arguments);
     };
 
-    util.inherits(Unicorn, generators.Base);
+    util.inherits(this.StubGenerator, yeoman.Base);
+  });
 
+  describe('#createGenerator', function () {
+
+    it('create a new generator', function () {
+      var generator = helpers.createGenerator('unicorn:app', [
+        [this.StubGenerator, 'unicorn:app']
+      ]);
+      assert.ok(generator instanceof this.StubGenerator);
     });
 
-  describe('helpers.createGenerator', function () {
-
-    it('with args params', function () {
+    it('pass args params to the generator', function () {
       helpers.createGenerator('unicorn:app', [
-        [Unicorn, 'unicorn:app']
+        [this.StubGenerator, 'unicorn:app']
       ], ['temp']);
-      // assert.ok();
       assert.deepEqual(this.args, ['temp']);
     });
 
-    it('with options param', function () {
+    it('pass options param to the generator', function () {
       helpers.createGenerator('unicorn:app', [
-        [Unicorn, 'unicorn:app']
+        [this.StubGenerator, 'unicorn:app']
       ], ['temp'], {ui: 'tdd'});
       assert.equal(this.options.ui, 'tdd');
     });
