27b25f6eedcc1e4a6407d401aeb652737584d09e
Simon Boudrias <simon.boudrias@cossette.com>
2013-12-12 15:52:10 -0500

    Remove env#remote disabled tests
    env#remote have been removed from core in 0.14 release

--- test/remote.js
+++ test/remote.js
@@ -109,18 +109,4 @@ describe('yeoman.base#remote', function () {
     });
   });
 
-  describe.skip('remote package', function () {
-    this.timeout(0);
-
-    it('env.remote(name)', function (done) {
-      var env = generators();
-      env.remote('mklabs/generators#generator-backbone', done);
-    });
-
-    it('init from remote', function (done) {
-      generators(['mklabs/generators#generator-angular', 'mklabs/generators#generator-chromeapp'])
-        .run(done);
-    });
-  });
-
 });
