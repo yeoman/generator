313da362a663ca36cffb23f462728ef16f4526cd
Simon Boudrias <simon.boudrias@cossette.com>
2013-12-12 16:44:08 -0500

    Add Benchmark.js suite for the Env module

---
+++ benchmark/env.js
@@ -0,0 +1,25 @@
+var generators = require('..');
+var Benchmark = require('benchmark');
+var chalk = require('chalk');
+var table = require('text-table');
+
+var suite = new Benchmark.Suite();
+
+suite
+  .add('Env#lookup', function () {
+    generators().lookup('*:*');
+  });
+
+suite.on('complete', function (e) {
+  var tests = e.currentTarget;
+  var nbr = tests.length
+  var t = [
+    [ 'test', 'ops/sec' ]
+  ];
+  while( nbr-- ) {
+    t.push([ tests[nbr].name, tests[nbr].hz ]);
+  }
+  console.log(table(t));
+});
+
+suite.run({ async: true });
