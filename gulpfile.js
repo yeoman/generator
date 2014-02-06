aa7a8e1184243600eb5191f36fa4bf029e28bd43
Simon Boudrias <admin@simonboudrias.com>
2014-02-05 22:11:38 -0500

    Update Gulp and fail build when jshint reports errors

--- gulpfile.js
+++ gulpfile.js
@@ -29,27 +29,26 @@ gulp.task('static', function () {
     'gulpfile.js'
   ]).pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
     .pipe(jscs());
 });
 
-gulp.task('default', function () {
-  gulp.run('static');
-
-  gulp.run('test', function () {
-    var fileContents = fs.readFileSync(path.join(__dirname, 'coverage/lcov.info'), 'utf8');
-    coveralls.getOptions(function (err, opt) {
+gulp.task('coveralls', ['test'], function () {
+  var fileContents = fs.readFileSync(path.join(__dirname, 'coverage/lcov.info'), 'utf8');
+  coveralls.getOptions(function (err, opt) {
+    if (err) throw err;
+    coveralls.convertLcovToCoveralls(fileContents, opt, function (err, data) {
       if (err) throw err;
-      coveralls.convertLcovToCoveralls(fileContents, opt, function (err, data) {
-        if (err) throw err;
-        coveralls.sendToCoveralls(data, function (err, response, body) {
-          var status = JSON.parse(body);
-          if (!body.error) {
-            console.log('Coveralls: Success');
-          } else {
-            console.log('Coveralls: Error ' + status.message);
-          }
-        });
+      coveralls.sendToCoveralls(data, function (err, response, body) {
+        var status = JSON.parse(body);
+        if (!body.error) {
+          console.log('Coveralls: Success');
+        } else {
+          console.log('Coveralls: Error ' + status.message);
+        }
       });
     });
   });
 });
+
+gulp.task('default', ['static', 'test', 'coveralls']);
