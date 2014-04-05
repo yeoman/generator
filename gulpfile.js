06c35964102a0b7ab18ea35d8d7c03eb0146a6ab
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-06 00:13:58 +0200

    code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,12 @@
 'use strict';
+var fs = require('fs');
+var path = require('path');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var istanbul = require('gulp-istanbul');
 var coveralls = require('gulp-coveralls');
-var fs = require('fs');
-var path = require('path');
-
-gulp.task('test', function (cb) {
-  gulp.src(['lib/**/*.js', 'main.js'])
-    .pipe(istanbul())
-    .on('end', function () {
-      gulp.src(['test/*.js'])
-        .pipe(mocha({ reporter: 'spec', timeout: 100000 }))
-        .pipe(istanbul.writeReports())
-        .on('end', cb);
-    });
-});
 
 gulp.task('static', function () {
   return gulp.src([
@@ -27,10 +16,28 @@ gulp.task('static', function () {
     'main.js',
     'doc.js',
     'gulpfile.js'
-  ]).pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jscs());
+  ])
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'))
+  .pipe(jscs());
+});
+
+gulp.task('test', function (cb) {
+  gulp.src([
+    'lib/**/*.js',
+    'main.js'
+  ])
+  .pipe(istanbul())
+  .on('end', function () {
+    gulp.src(['test/*.js'])
+      .pipe(mocha({
+        reporter: 'spec',
+        timeout: 100000
+      }))
+      .pipe(istanbul.writeReports())
+      .on('end', cb);
+  });
 });
 
 gulp.task('coveralls', ['test'], function () {
