9c9667778131f3503436805a4866d98c8b2ec8e6
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-06-23 15:11:46 +0100

    Added ability to clone non github repos

--- test/remote.js
+++ test/remote.js
@@ -14,100 +14,199 @@ describe('yeoman.base#remote', function () {
     this.dummy = env.create('dummy');
   });
 
-  describe('callback argument remote#copy', function () {
+  describe('github', function () {
+    describe('callback argument remote#copy', function () {
 
-    before(function (done) {
-      this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
-        this.dummy.foo = 'foo';
-        remote.copy('test/fixtures/template.jst', 'remote/template.js');
-        this.dummy.conflicter.resolve(done);
-      }.bind(this), true);
+      before(function (done) {
+        this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
+          this.dummy.foo = 'foo';
+          remote.copy('test/fixtures/template.jst', 'remote-githug/template.js');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a file from a remote resource', function (done) {
+        fs.readFile('remote-githug/template.js', function (err, data) {
+          assert.equal(data.toString(), 'var foo = \'foo\';\n');
+          done();
+        });
+      });
     });
 
-    it('copy a file from a remote resource', function (done) {
-      fs.readFile('remote/template.js', function (err, data) {
-        assert.equal(data.toString(), 'var foo = \'foo\';\n');
-        done();
+    describe('callback argument remote#bulkCopy', function () {
+
+      before(function (done) {
+        this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
+          remote.bulkCopy('test/fixtures/foo-template.js', 'remote-githug/foo-template.js');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a file from a remote resource', function (done) {
+        fs.stat('remote-githug/foo-template.js', done);
+      });
+
+      it('doesn\'t process templates on bulkCopy', function (done) {
+        fs.readFile('remote-githug/foo-template.js', function (err, data) {
+          assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
+          done();
+        });
+      });
+    });
+
+    describe('callback argument remote#directory', function () {
+
+      before(function (done) {
+        this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
+          remote.directory('test/generators', 'remote-githug/generators');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a directory from a remote resource', function (done) {
+        fs.stat('remote-githug/generators/test-angular.js', done);
+      });
+
+    });
+
+    describe('callback argument remote#bulkDirectory', function () {
+
+      before(function (done) {
+        this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
+          remote.bulkDirectory('test/fixtures', 'remote-githug/fixtures');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a directory from a remote resource', function (done) {
+        fs.stat('remote-githug/fixtures/foo.js', done);
+      });
+
+      it('doesn\'t process templates on bulkDirectory', function (done) {
+        fs.readFile('remote-githug/fixtures/foo-template.js', function (err, data) {
+          assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
+          done();
+        });
+      });
+
+    });
+
+    describe('callback argument remote fileUtils Environment instances', function () {
+      before(function (done) {
+        this.cachePath = path.join(this.dummy.cacheRoot(), 'yeoman/generator/master');
+        this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
+          this.remoteArg = remote;
+          done();
+        }.bind(this));
+      });
+
+      it('.src is scoped to cachePath', function () {
+        assert.equal(this.remoteArg.src.fromBase('.'), this.cachePath);
+        assert.equal(this.remoteArg.src.fromDestBase('.'), this.dummy.destinationRoot());
+      });
+
+      it('.dest is scoped to destinationRoot', function () {
+        assert.equal(this.remoteArg.dest.fromBase('.'), this.dummy.destinationRoot());
+        assert.equal(this.remoteArg.dest.fromDestBase('.'), this.cachePath);
       });
     });
   });
 
-  describe('callback argument remote#bulkCopy', function () {
+  describe('absolute', function () {
+    describe('callback argument remote#copy', function () {
 
-    before(function (done) {
-      this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
-        remote.bulkCopy('test/fixtures/foo-template.js', 'remote/foo-template.js');
-        this.dummy.conflicter.resolve(done);
-      }.bind(this), true);
-    });
-
-    it('copy a file from a remote resource', function (done) {
-      fs.stat('remote/foo-template.js', done);
-    });
-
-    it('doesn\'t process templates on bulkCopy', function (done) {
-      fs.readFile('remote/foo-template.js', function (err, data) {
-        assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
-        done();
+      before(function (done) {
+        this.dummy.remote('https://github.com/yeoman/generator/archive/master.zip', function (err, remote) {
+          this.dummy.foo = 'foo';
+          remote.copy('test/fixtures/template.jst', 'remote-absolute/template.js');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
       });
-    });
-  });
 
-  describe('callback argument remote#directory', function () {
-
-    before(function (done) {
-      this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
-        remote.directory('test/generators', 'remote/generators');
-        this.dummy.conflicter.resolve(done);
-      }.bind(this), true);
-    });
-
-    it('copy a directory from a remote resource', function (done) {
-      fs.stat('remote/generators/test-angular.js', done);
-    });
-
-  });
-
-  describe('callback argument remote#bulkDirectory', function () {
-
-    before(function (done) {
-      this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
-        remote.bulkDirectory('test/fixtures', 'remote/fixtures');
-        this.dummy.conflicter.resolve(done);
-      }.bind(this), true);
-    });
-
-    it('copy a directory from a remote resource', function (done) {
-      fs.stat('remote/fixtures/foo.js', done);
-    });
-
-    it('doesn\'t process templates on bulkDirectory', function (done) {
-      fs.readFile('remote/fixtures/foo-template.js', function (err, data) {
-        assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
-        done();
+      it('copy a file from a remote resource', function (done) {
+        fs.readFile('remote-absolute/template.js', function (err, data) {
+          assert.equal(data.toString(), 'var foo = \'foo\';\n');
+          done();
+        });
       });
     });
 
+    describe('callback argument remote#bulkCopy', function () {
+
+      before(function (done) {
+        this.dummy.remote('https://github.com/yeoman/generator/archive/master.zip', function (err, remote) {
+          remote.bulkCopy('test/fixtures/foo-template.js', 'remote-absolute/foo-template.js');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a file from a remote resource', function (done) {
+        fs.stat('remote-absolute/foo-template.js', done);
+      });
+
+      it('doesn\'t process templates on bulkCopy', function (done) {
+        fs.readFile('remote-absolute/foo-template.js', function (err, data) {
+          assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
+          done();
+        });
+      });
+    });
+
+    describe('callback argument remote#directory', function () {
+
+      before(function (done) {
+        this.dummy.remote('https://github.com/yeoman/generator/archive/master.zip', function (err, remote) {
+          remote.directory('test/generators', 'remote-absolute/generators');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a directory from a remote resource', function (done) {
+        fs.stat('remote-absolute/generators/test-angular.js', done);
+      });
+
+    });
+
+    describe('callback argument remote#bulkDirectory', function () {
+
+      before(function (done) {
+        this.dummy.remote('https://github.com/yeoman/generator/archive/master.zip', function (err, remote) {
+          remote.bulkDirectory('test/fixtures', 'remote-absolute/fixtures');
+          this.dummy.conflicter.resolve(done);
+        }.bind(this), true);
+      });
+
+      it('copy a directory from a remote resource', function (done) {
+        fs.stat('remote-absolute/fixtures/foo.js', done);
+      });
+
+      it('doesn\'t process templates on bulkDirectory', function (done) {
+        fs.readFile('remote-absolute/fixtures/foo-template.js', function (err, data) {
+          assert.equal(data.toString(), 'var <%= foo %> = \'<%= foo %>\';\n');
+          done();
+        });
+      });
+
+    });
+
+    describe('callback argument remote fileUtils Environment instances', function () {
+      before(function (done) {
+        this.cachePath = path.join(this.dummy.cacheRoot(), 'httpsgithubcomyeomangeneratorarchivemasterzip');
+        this.dummy.remote('https://github.com/yeoman/generator/archive/master.zip', function (err, remote) {
+          this.remoteArg = remote;
+          done();
+        }.bind(this));
+      });
+
+      it('.src is scoped to cachePath', function () {
+        assert.equal(this.remoteArg.src.fromBase('.'), this.cachePath);
+        assert.equal(this.remoteArg.src.fromDestBase('.'), this.dummy.destinationRoot());
+      });
+
+      it('.dest is scoped to destinationRoot', function () {
+        assert.equal(this.remoteArg.dest.fromBase('.'), this.dummy.destinationRoot());
+        assert.equal(this.remoteArg.dest.fromDestBase('.'), this.cachePath);
+      });
+    });
   });
-
-  describe('callback argument remote fileUtils Environment instances', function () {
-    before(function (done) {
-      this.cachePath = path.join(this.dummy.cacheRoot(), 'yeoman/generator/master');
-      this.dummy.remote('yeoman', 'generator', 'master', function (err, remote) {
-        this.remoteArg = remote;
-        done();
-      }.bind(this));
-    });
-
-    it('.src is scoped to cachePath', function () {
-      assert.equal(this.remoteArg.src.fromBase('.'), this.cachePath);
-      assert.equal(this.remoteArg.src.fromDestBase('.'), this.dummy.destinationRoot());
-    });
-
-    it('.dest is scoped to destinationRoot', function () {
-      assert.equal(this.remoteArg.dest.fromBase('.'), this.dummy.destinationRoot());
-      assert.equal(this.remoteArg.dest.fromDestBase('.'), this.cachePath);
-    });
-  });
-
 });
