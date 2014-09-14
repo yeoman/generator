64caaf308189ba70b440b29d457e1e6207d0cb25
Simon Boudrias <admin@simonboudrias.com>
2014-09-13 23:27:08 -0700

    Add tests covering wiring (re #504)

--- test/wiring.js
+++ test/wiring.js
@@ -6,7 +6,7 @@ var wiring = require('../lib/actions/wiring');
 var yeoman = require('..');
 var assert = yeoman.assert;
 
-describe('yeoman.generator.lib.actions.wiring', function () {
+describe('Yeoman actions/wiring', function () {
   before(function () {
     this.fixtures = path.join(__dirname, 'fixtures');
   });
@@ -122,5 +122,104 @@ describe('yeoman.generator.lib.actions.wiring', function () {
     var html = '<html><body><p></p></body></html>';
     var expected = '<html><body><p>Hello World!你好世界!ハローワールド!안녕 하세요 세계!</p></body></html>';
     assert.equal(wiring.domUpdate(html, 'p', 'Hello World!你好世界!ハローワールド!안녕 하세요 세계!', 'p'), expected);
+  });
+
+  describe('#appendFiles()', function () {
+    it('work the same using the object syntax', function () {
+      var html = '<html><body></body></html>';
+      var res = wiring.appendFiles(html, 'js', 'out/file.js', ['in/file1.js', 'in/file2.js']);
+      var res2 = wiring.appendFiles({
+        html: html,
+        fileType: 'js',
+        optimizedPath: 'out/file.js',
+        sourceFileList: ['in/file1.js', 'in/file2.js']
+      });
+
+      assert.equal(res, res2);
+    });
+
+    it('work with css file', function () {
+      var html = '<html><head></head></html>';
+      var res = wiring.appendFiles(html, 'css', 'out/file.css', ['in/file1.css', 'in/file2.css']);
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'css_block.html'), 'utf-8').trim();
+
+      assert.textEqual(res, fixture);
+    });
+
+    it('work with attributes params', function () {
+      var html = '<html><body></body></html>';
+      var res = wiring.appendFiles({
+        html: html,
+        fileType: 'js',
+        optimizedPath: 'out/file.js',
+        sourceFileList: ['in/file1.js', 'in/file2.js'],
+        attrs: {
+          'data-test': 'my-attr'
+        }
+      });
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'js_block_with_attr.html'), 'utf-8').trim();
+
+      assert.textEqual(res, fixture);
+    });
+  });
+
+  describe('#appendScripts()', function () {
+    it('append scripts', function () {
+      var html = '<html><body></body></html>';
+      var res = wiring.appendScripts(html, 'out/file.js', ['in/file1.js', 'in/file2.js']);
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'js_block.html'), 'utf-8').trim();
+      assert.textEqual(res, fixture);
+    });
+  });
+
+  describe('#removeScript()', function () {
+    it('remove script', function () {
+      var withScript = '<html><body><script src="file1.js"></script></body></html>';
+      var html = '<html><body></body></html>';
+
+      var res = wiring.removeScript(withScript, 'file1.js');
+      assert.textEqual(res, html);
+    });
+  });
+
+  describe('#appendStyles()', function () {
+    it('append styles', function () {
+      var html = '<html><head></head></html>';
+      var res = wiring.appendStyles(html, 'out/file.css', ['in/file1.css', 'in/file2.css']);
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'css_block.html'), 'utf-8').trim();
+
+      assert.textEqual(res, fixture);
+    });
+  });
+
+  describe('#removeStyle()', function () {
+    it('should remove style', function () {
+      var withStyle = '<html><head><link rel="stylesheet" href="file1.css"></head></html>';
+      var html = '<html><head></head></html>';
+
+      var res = wiring.removeStyle(withStyle, 'file1.css');
+
+      assert.textEqual(res, html);
+    });
+  });
+
+  describe('#appendScriptsDir()', function () {
+    it('append scripts directory', function () {
+      var html = '<html><body></body></html>';
+      var res = wiring.appendScriptsDir(html, 'out/file.js', path.join(__dirname, 'fixtures/dir-fixtures'));
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'js_block_dir.html'), 'utf-8').trim();
+
+      assert.textEqual(res, fixture);
+    });
+  });
+
+  describe('#appendStylesDir()', function () {
+    it('should append styles directory', function () {
+      var html = '<html><head></head></html>';
+      var res = wiring.appendStylesDir(html, 'out/file.css', path.join(__dirname, 'fixtures/dir-css-fixtures'));
+      var fixture = fs.readFileSync(path.join(this.fixtures, 'css_block_dir.html'), 'utf-8').trim();
+
+      assert.textEqual(res, fixture);
+    });
   });
 });
