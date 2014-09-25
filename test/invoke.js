79e855329c4891517a39bf806c05948adefce8ab
Simon Boudrias <admin@simonboudrias.com>
2014-09-24 23:56:10 -0700

    Fix Base#hookFor() method and cleanup invoke fix
    Add Environment#duplicate() to create performant copy of a complete
    environment.

--- test/invoke.js
+++ test/invoke.js
@@ -8,15 +8,17 @@ var assert = yo.assert;
 describe('Base#invoke()', function () {
   beforeEach(function () {
     this.env = yo();
-    this.gen = new (helpers.createDummyGenerator())({
+    this.Generator = helpers.createDummyGenerator();
+    this.gen = new this.Generator({
       namespace: 'foo:lab',
       resolved: 'path/',
       env: this.env,
       'skip-install': true
     });
-    this.env.registerStub(yo.generators.Base.extend({
+    this.SubGen = yo.generators.Base.extend({
       exec: function () { this.stubGenRunned = true; }
-    }), 'foo:bar');
+    });
+    this.env.registerStub(this.SubGen, 'foo:bar');
   });
 
   it('invoke available generators', function (done) {
@@ -40,20 +42,34 @@ describe('Base#invoke()', function () {
 
   it('works when invoked from runLoop', function (done) {
     var stubGenFinished = false;
+    var running = 0;
 
-    this.gen.constructor.prototype.bar = function () {
+    var asyncFunc = function () {
       var cb = this.async();
-      var invoked = this.invoke('foo:bar',
-        { options: { 'skip-install': true } },
-        function () {
-          stubGenFinished = true;
-          assert(invoked.stubGenRunned, 'runned');
-          cb();
-        });
+      running++;
+      setTimeout(function () {
+        assert.equal(running, 1);
+        running--;
+        cb();
+      }, 5);
+    };
+
+    this.SubGen.prototype.asyncFunc = asyncFunc;
+    this.gen.constructor.prototype.asyncFunc = asyncFunc;
+
+    this.gen.constructor.prototype.initializing = function () {
+      var cb = this.async();
+      var invoked = this.invoke('foo:bar', {
+        options: { 'skip-install': true }
+      }, function () {
+        stubGenFinished = true;
+        assert(invoked.stubGenRunned, 'Stub generator should have runned');
+        cb();
+      });
     };
 
     this.gen.run(function () {
-      assert(stubGenFinished, 'finished');
+      assert(stubGenFinished, 'invoke callback should have been called');
       done();
     });
   });
