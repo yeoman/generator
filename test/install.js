b8234b53ab1d8edd99b8e5a3d342fb3260415571
Simon Boudrias <admin@simonboudrias.com>
2014-11-04 23:03:27 -0800

    Only run installations commands once. Fix #668

--- test/install.js
+++ test/install.js
@@ -33,30 +33,73 @@ describe('generators.Base (actions/install mixin)', function () {
           PATH: '/path/to/bin'
         }
       };
+      var callbackSpy = sinon.spy();
       //args: installer, paths, options, cb
-      this.dummy.runInstall('nestedScript', ['path1', 'path2'], spawnEnv, done);
-      sinon.assert.calledWithExactly(this.spawnCommandStub, 'nestedScript', ['install', 'path1', 'path2'], spawnEnv);
+      this.dummy.runInstall('nestedScript', ['path1', 'path2'], spawnEnv, callbackSpy);
+      this.dummy.run(function () {
+        sinon.assert.calledWithExactly(
+          this.spawnCommandStub,
+          'nestedScript',
+          ['install', 'path1', 'path2'],
+          spawnEnv
+        );
+        sinon.assert.calledOnce(callbackSpy);
+        done();
+      }.bind(this));
     });
   });
 
   describe('#bowerInstall()', function () {
-    it('spawn a bower process', function (done) {
-      this.dummy.bowerInstall(null, done);
-      sinon.assert.calledOnce(this.spawnCommandStub);
-      sinon.assert.calledWithExactly(this.spawnCommandStub, 'bower', ['install'], {});
+    it('spawn a bower process once per commands', function (done) {
+      this.dummy.bowerInstall();
+      this.dummy.bowerInstall();
+      this.dummy.run(function () {
+        sinon.assert.calledOnce(this.spawnCommandStub);
+        sinon.assert.calledWithExactly(this.spawnCommandStub, 'bower', ['install'], {});
+        done();
+      }.bind(this));
     });
 
     it('spawn a bower process with formatted options', function (done) {
-      this.dummy.bowerInstall('jquery', { saveDev: true }, done);
-      sinon.assert.calledOnce(this.spawnCommandStub);
-      sinon.assert.calledWithExactly(this.spawnCommandStub, 'bower', ['install', 'jquery', '--save-dev'], { saveDev: true });
+      this.dummy.bowerInstall('jquery', { saveDev: true }, function () {
+        sinon.assert.calledOnce(this.spawnCommandStub);
+        sinon.assert.calledWithExactly(
+          this.spawnCommandStub,
+          'bower',
+          ['install', 'jquery', '--save-dev'],
+          { saveDev: true }
+        );
+        done();
+      }.bind(this));
+      this.dummy.run();
     });
   });
 
   describe('#npmInstall()', function () {
-    it('run without callback', function () {
+    it('spawn an install process once per commands', function (done) {
+      this.dummy.npmInstall();
+      this.dummy.npmInstall();
+      this.dummy.run(function () {
+        sinon.assert.calledOnce(this.spawnCommandStub);
+        sinon.assert.calledWithExactly(this.spawnCommandStub, 'npm', ['install'], {});
+        done();
+      }.bind(this));
+    });
+
+    it('run without callback', function (done) {
       this.dummy.npmInstall('yo', { save: true });
-      sinon.assert.calledOnce(this.spawnCommandStub);
+      this.dummy.run(function () {
+        sinon.assert.calledOnce(this.spawnCommandStub);
+        done();
+      }.bind(this));
+    });
+
+    it('run with callback', function (done) {
+      this.dummy.npmInstall('yo', { save: true }, function () {
+        sinon.assert.calledOnce(this.spawnCommandStub);
+        done();
+      }.bind(this));
+      this.dummy.run();
     });
   });
 
@@ -68,23 +111,30 @@ describe('generators.Base (actions/install mixin)', function () {
         sinon.assert.calledWithExactly(this.spawnCommandStub, 'npm', ['install'], {});
         done();
       }.bind(this));
+      this.dummy.run();
     });
 
-    it('doesn\'t spawn anything with skipInstall', function () {
+    it('does not spawn anything with skipInstall', function (done) {
       this.dummy.installDependencies({ skipInstall: true });
-      sinon.assert.notCalled(this.spawnCommandStub);
+      this.dummy.run(function () {
+        sinon.assert.notCalled(this.spawnCommandStub);
+        done();
+      }.bind(this));
     });
 
     it('call callback if skipInstall', function (done) {
       this.dummy.installDependencies({ skipInstall: true, callback: done });
+      this.dummy.run();
     });
 
     it('execute a callback after installs', function (done) {
       this.dummy.installDependencies({ callback: done });
+      this.dummy.run();
     });
 
     it('accept and execute a function as its only argument', function (done) {
       this.dummy.installDependencies(done);
+      this.dummy.run();
     });
   });
 });
