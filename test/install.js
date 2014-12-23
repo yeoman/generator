3699326ea7b428d9963af809d270212dc1d77c24
Stefan Buck <github@stefanbuck.com>
2014-12-23 01:03:44 +0100

    Add method to terminate install process

--- test/install.js
+++ test/install.js
@@ -11,7 +11,11 @@ var asyncStub = {
     if (key === 'exit') {
       cb();
     }
+    this[key] = cb;
     return asyncStub;
+  },
+  kill: function () {
+    this.exit(130);
   }
 };
 
@@ -135,6 +139,40 @@ describe('generators.Base (actions/install mixin)', function () {
     it('accept and execute a function as its only argument', function (done) {
       this.dummy.installDependencies(done);
       this.dummy.run();
+    });
+  });
+
+  describe('#terminateInstall()', function () {
+
+    it('terminate at start', function (done) {
+      var neverCalled = sinon.stub();
+      this.dummy.on('install:terminated', done);
+      this.dummy.on('bowerInstall:end', neverCalled);
+      this.dummy.on('npmInstall:end', neverCalled);
+      this.dummy.run(neverCalled);
+      this.dummy.terminateInstall();
+      this.dummy.installDependencies();
+    });
+
+    it('terminate after bower', function (done) {
+      var neverCalled = sinon.stub();
+      this.dummy.on('install:terminated', done);
+      this.dummy.on('bowerInstall:end', this.dummy.terminateInstall);
+      this.dummy.on('npmInstall:end', neverCalled);
+      this.dummy.run(neverCalled);
+      this.dummy.installDependencies();
+    });
+
+    it('execute run after terminate', function (done) {
+      var installEnd = sinon.stub();
+      this.dummy.on('bowerInstall:end', installEnd);
+      this.dummy.on('npmInstall:end', installEnd);
+      this.dummy.terminateInstall();
+      this.dummy.installDependencies();
+      this.dummy.run(function () {
+        sinon.assert.calledTwice(installEnd);
+        done();
+      });
     });
   });
 });
