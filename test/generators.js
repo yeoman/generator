36015315e4fcdf6d9add0811069f5cff1765d5d9
Simon Boudrias <admin@simonboudrias.com>
2016-11-25 22:16:06 -0800

    Get rid of yeoman-environment proxying

--- test/generators.js
+++ test/generators.js
@@ -1,35 +1,14 @@
 /*global describe, before, beforeEach, it */
 'use strict';
-var path = require('path');
-var os = require('os');
 var events = require('events');
 var Environment = require('yeoman-environment');
 var generators = require('../');
 var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
-var tmpdir = path.join(os.tmpdir(), 'yeoman-generators');
 
 describe('Generators module', function () {
-  before(helpers.setUpTestDirectory(tmpdir));
-
-  describe('module', function () {
-    it('initialize new Environments', function () {
-      assert.ok(generators() instanceof Environment);
-      assert.notEqual(generators(), generators());
-    });
-
-    it('pass arguments to the Environment constructor', function () {
-      var args = ['model', 'Post'];
-      var opts = { help: true };
-      var env = generators(args, opts);
-      assert.deepEqual(env.arguments, args);
-      assert.deepEqual(env.options, opts);
-    });
-  });
-
   describe('generators.Base', function () {
     beforeEach(function () {
-      this.env = generators();
+      this.env = Environment.createEnv();
       this.generator = new generators.Base({
         env: this.env,
         resolved: 'test'
