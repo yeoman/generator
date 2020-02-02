82a4eb837afbce2eb6418ec0f569f070aaced70a
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-02-02 16:24:09 -0200

    Implement localConfigOnly option. (#1161)
    Reduces unwanted outside interference.
    
    - Don't find .yo-rc.json at parent folders.
    - Simulate a global config at current folder.

--- test/generators.js
+++ test/generators.js
@@ -2,12 +2,18 @@
 const EventEmitter = require('events');
 const Environment = require('yeoman-environment');
 const assert = require('yeoman-assert');
+const path = require('path');
+const os = require('os');
+
 const Base = require('..');
 
 describe('Generators module', () => {
+  beforeEach(function() {
+    this.env = Environment.createEnv();
+  });
+
   describe('Base', () => {
     beforeEach(function() {
-      this.env = Environment.createEnv();
       this.generator = new Base({
         env: this.env,
         resolved: 'test'
@@ -21,5 +27,28 @@ describe('Generators module', () => {
       this.generator.on('yay-o-man', done);
       this.generator.emit('yay-o-man');
     });
+  });
+
+  it('without localConfigOnly option', function() {
+    this.generator = new Base({
+      env: this.env,
+      resolved: 'test'
+    });
+    assert.equal(
+      path.join(os.homedir(), '.yo-rc-global.json'),
+      this.generator._globalConfig.path
+    );
+  });
+
+  it('with localConfigOnly option', function() {
+    this.generator = new Base({
+      env: this.env,
+      resolved: 'test',
+      localConfigOnly: true
+    });
+    assert.equal(
+      path.join(this.env.cwd, '.yo-rc-global.json'),
+      this.generator._globalConfig.path
+    );
   });
 });
