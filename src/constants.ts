5969f776272e7eb0d00993e8d0057618f6975940
Marcelo Shima <marceloshima@gmail.com>
2023-10-16 00:28:16 -0300

    bump environment requirement

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DESTINATION_ROOT_CHANGE_EVENT = 'destinationRootChange';
 
-export const requiredEnvironmentVersion = '3.18.4';
+export const requiredEnvironmentVersion = '4.0.0-rc.0';
